5ca2c3b7f8372e8c703f1105823675c4c36de995
Jens Östlund <jostlund@gmail.com>
2015-09-27 01:18:55 +0200

    Applicative way

--- src/Cedict/Parser.hs
+++ src/Cedict/Parser.hs
@@ -1,27 +1,47 @@
 {-# LANGUAGE FlexibleContexts #-}
 
-module Cedict.Parser (line, lines, comment) where
+module Cedict.Parser (parse, Entry) where
 
-import           Control.Applicative
-import           Prelude             hiding (lines)
-import           Text.Parsec         ((<?>))
-import qualified Text.Parsec         as P
+import           Control.Applicative           ((*>), (<$>), (<*>))
+import           Prelude                       hiding (lines)
+import           Text.Parsec                   ((<?>))
+import           Text.ParserCombinators.Parsec (ParseError, Parser, anyChar,
+                                                between, char, digit, eof,
+                                                letter, many, many1, manyTill,
+                                                noneOf, oneOf, skipMany, space,
+                                                (<|>))
+import qualified Text.ParserCombinators.Parsec as P
 
+data Entry = Entry { traditional :: String
+                   , simplified  :: String
+                   , pinyin      :: String
+                   , translation :: String} deriving (Show)
 
-comment :: P.Parsec String () ()
-comment = do
-  P.char '#'
-  P.skipMany $ P.noneOf "\r\n"
-  <?> "comment"
+parse :: String -> Either ParseError [Entry]
+parse = P.parse lines ""
 
-line :: P.Parsec String () String
-line = P.many $ P.noneOf "\r\n"
+lines :: Parser [Entry]
+lines = (:) <$> entryLine <*> (many entryLine) <* skippable <* eof <?> "lines"
+        where entryLine = id <$> skippable *> line
+              skippable = skipMany (comment <|> eol)
 
-lines :: P.Parsec String () [String]
-lines = do
-  l <- line
-  ls <- P.many $ do
-          P.newline
-          line
-  P.eof
-  return (l:ls)
+line :: Parser Entry
+line = Entry <$> entry <*> (delim *> entry) <*> (delim *> pron) <*> (delim *> trans) <?> "line"
+       where delim = skipMany space
+
+entry :: Parser String
+entry = many1 (letter <|> digit) <?> "entry"
+
+trans :: Parser String
+trans = (:) <$> char '/' <*> (manyTill anyChar (eol <|> comment)) <?> "translation"
+
+pron :: Parser String
+pron = between (char '[') (char ']') (many1 (letter <|> digit <|> space)) <?> "pronunciation"
+
+comment :: Parser ()
+comment = id <$> (char '#' *> (skipMany $ noneOf "\r\n")) <?> "comment"
+
+eol :: Parser ()
+eol = do oneOf "\n\r"
+         return ()
+      <?> "end of line"
