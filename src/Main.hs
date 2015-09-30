6eab90a12c9155bb14262eb0a1a33a6b872989b2
Jens Östlund <jostlund@gmail.com>
2015-10-01 00:38:10 +0200

    Takes GZipped file as input

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
 import           Cedict.Entry                  (Entry)
 import           Cedict.Parser                 (entries)
+import           Codec.Compression.GZip        (decompress)
+import           Data.ByteString.Char8         (unpack)
+import qualified Data.ByteString.Lazy.Char8    as LBS
 import           Data.List                     (intercalate)
 import           System.Environment            (getArgs)
-import           Text.Parsec.String            (parseFromFile)
+import           Text.Parsec                   (parse)
 import           Text.ParserCombinators.Parsec (ParseError)
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    (file:_) -> parseFromFile entries file >>= either report printResult
+    (file:_) -> do
+              content <- fmap decompress (LBS.readFile file)
+              case parse entries file (bsToString content) of
+                Left err -> report err
+                Right es -> printResult es
     _ -> putStrLn "ERROR: Please specify a file path!"
 
-report :: ParseError -> IO ()
-report err = putStrLn $ "Error: " ++ show err
-
-printResult :: [Entry] -> IO ()
-printResult res =
-    putStrLn $ intercalate "\n" $ map show res
+    where bsToString = unpack . LBS.toStrict
+          report err = putStrLn $ "Error: " ++ show err
+          printResult r = putStrLn $ intercalate "\n" $ map show r
